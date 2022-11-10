db82422937dbb7fbbbb0c1a6fd921769515ca930
Adam Toy <adam.toy@rancherfederal.com>
2022-11-10 13:13:50 -0500

    add details

--- README.md
+++ README.md
@@ -1,40 +1,11 @@
-# Git-HTTP-backend
-A hacky, insecure but smart-http server using nginx as a frontend. No authentication, no SSL, push is free-for-all.
+# Carbide Umbrella Implementation
 
-> _disclaimer_ this is not intended for production use. Written to ease self-bootstrapping GitOps goodness.
+This will roll out the Carbide charts downstream
 
-![git-http-backend](static/thisisfine.png?raw=true)
+## How-To
 
+To roll it out:
 
-## Usage:
-
-Running the server: 
-```bash
-docker run --rm 
-  -p 8080:80 \
-  -v /path/to/host/gitdir:/git \
-  -e "INIT_REPOS=myrepo1,myrepo2" \ #Comma-delimited list of repos to create at initialization 
-  bgulla/git-http-backend
-```
-
-Cloning a repo
-```bash
-git clone http://localhost:8080/git/myrepo1.git
-echo "profit!"
-```
-
-## Importing existing `git` repos
-You can serve existing repos from disk! If you would like to serve up existing git repositories from your `git-http-server`, just make sure that they are included in whatever folder you mount to `/git`. 
-
-Note: these repos will be referenced by their folder name so `/git/foobar` will clone with `git clone http://localhost:8080/git/foobar` without the `.git` suffix.  
-
-## Interactive Initialization
-You will need to set the `INIT_REPOS` env var in order to self-bootstrap a repository within the /git folder. If you want to manually create the repo while the container is running, do the following:
-
-```bash
-cd /git
-git init --bare test.git
-cd test.git
-git config http.receivepack true
-```
-
+1. Switch your Kubectl context to your local Rancher cluster.
+2. Run the `initialize-local.sh`.
+3. Check your Continuous Delivery in Rancher (both for `fleet-local` and `fleet-default` namespaces.)
---
+++ initialize-local.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+########################################################################
+# NOTES: Run this on your local 'rancher' cluster.                     #
+# This will initialize all things Carbide on your downstream clusters. #
+########################################################################
+
+echo "Creating the Carbide namespace.."
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: carbide
+  labels:
+    name: carbide
+EOF
+
+echo "Creating Git chart repo deployment.."
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: carbide-charts
+  namespace: carbide
+  labels:
+    app: carbide-charts
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: carbide-charts
+  template:
+    metadata:
+      labels:
+        app: carbide-charts
+    spec:
+      containers:
+      - name: carbide-charts
+        image: harbor.atoy.dev/public/carbide-charts:1.0
+        ports:
+        - name: http
+          containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: carbide-charts
+  namespace: carbide
+spec:
+  selector:
+    app: carbide-charts
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: http
+EOF
+
+echo "Creating Gitrepo to trigger umbrella flow.."
+cat <<EOF | kubectl apply -f -
+kind: GitRepo
+apiVersion: fleet.cattle.io/v1alpha1
+metadata:
+  name: carbide-charts
+  namespace: fleet-local
+spec:
+  repo: http://carbide-charts.carbide.svc.cluster.local/git/git-http-backend.git
+  targets:
+  - clusterSelector:
+      matchExpressions:
+      - key: provider.cattle.io
+        operator: NotIn
+        values:
+        - harvester
+  paths:
+  - "./carbide/umbrella"
+EOF
